c71c56b78f81f28e236a1c41a37f231a64dd7aed
Rafael Abreu <rafael_sabreu@hotmail.com>
2023-03-03 22:35:12 -0300

    Product

--- src/main/java/com/educandoweb/course/resources/ProductResource.java
+++ src/main/java/com/educandoweb/course/resources/ProductResource.java
@@ -9,25 +9,25 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.educandoweb.course.entities.Category;
-import com.educandoweb.course.service.CategoryService;
+import com.educandoweb.course.entities.Product;
+import com.educandoweb.course.service.ProductService;
 
 @RestController
-@RequestMapping(value = "/category")
-public class CategoryResource {
+@RequestMapping(value = "/product")
+public class ProductResource {
 	
 	@Autowired
-	private CategoryService service;
+	private ProductService service;
 
 	@GetMapping
-	public ResponseEntity<List<Category>> findAll() {
-		List<Category> list = service.findAll();
+	public ResponseEntity<List<Product>> findAll() {
+		List<Product> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 	
 	@GetMapping(value = "/{id}")
-	public ResponseEntity <Category> findById(@PathVariable Long id) {
-		Category obj = service.findById(id);
+	public ResponseEntity <Product> findById(@PathVariable Long id) {
+		Product obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
 }
