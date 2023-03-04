9e7fa1ec4e365223b924b3ae5329dbe4b931212f
Rafael Abreu <rafael_sabreu@hotmail.com>
2023-03-03 22:37:30 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/educandoweb/course/resources/ProductResource.java
+++ src/main/java/com/educandoweb/course/resources/ProductResource.java
@@ -13,7 +13,7 @@ import com.educandoweb.course.entities.Product;
 import com.educandoweb.course.service.ProductService;
 
 @RestController
-@RequestMapping(value = "/product")
+@RequestMapping(value = "/products")
 public class ProductResource {
 	
 	@Autowired
