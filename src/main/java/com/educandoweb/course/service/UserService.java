6f0b83af2051aa74c4cd9a691fe5ff4952b224a7
Rafael Abreu <rafael_sabreu@hotmail.com>
2023-03-20 16:06:40 -0300

    findById Exception

--- src/main/java/com/educandoweb/course/service/UserService.java
+++ src/main/java/com/educandoweb/course/service/UserService.java
@@ -26,8 +26,8 @@ public class UserService {
 	}
 	
 	public User findById(Long id) {
-		Optional<User> obj =repository.findById(id);
-		return obj.get();
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
